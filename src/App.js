fee153123476496f818dfd3bd4f9a3fb41099d63
Gabriel <vmgabriel96@gmail.com>
2020-08-22 22:48:37 -0500

    feat: add react material

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-// Librar
+// Libraries
 import React from 'react';
 
+// Material
+import {
+  Button,
+} from '@material-ui/core';
 
 const App = () => {
   return (
     <div className="App">
       <h2>Hello World</h2>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </div>
   );
-}
-
+};
 
 export default App;
